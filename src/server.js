13a1d2829763e8b5a55f46881b84b110a6fc4b15
khushi-kv <khushiverma1037@gmail.com>
2026-05-15 12:58:45 +0530

    feat: setup MongoDB Atlas connection with Mongoose and env configuration.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
-//server entry point 
+// server entry point
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDB } from "./config/db.js";
 
-const PORT=5001;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port http://localhost:${PORT}`);
-})
+
+// ✅ connect to MongoDB first
+connectDB();
+
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
